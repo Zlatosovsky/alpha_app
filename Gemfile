a7f2e5a996a49941c6d07527d56875c01cda7d10
Zlatosovsky <zlatosovsky@gmail.com>
2015-01-15 18:03:47 +0200

    Make a basic User model (including secure passwords)

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ ruby '2.1.5'
 
 gem 'rails', '4.1.8'
 gem 'bootstrap-sass', '2.3.2.0'
+gem 'bcrypt'
 
 group :development, :test do
   gem 'sqlite3'
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150109134352) do
+ActiveRecord::Schema.define(version: 20150114230552) do
+
+  create_table "girls", force: true do |t|
+    t.string   "name"
+    t.string   "rating"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
