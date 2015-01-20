9bb1918baa25a6ea1826d59396c1446ffe1abf45
Zlatosovsky <zlatosovsky@gmail.com>
2015-01-20 16:20:09 +0200

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+	factory :user do
+		name                  "Ivan Pypkin"
+		email                 "ivan@pypkin.com"
+		password              "123404321"
+		password_confirmation "123404321"
+	end
+end
